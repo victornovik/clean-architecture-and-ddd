7c7fb6b193b35f621916e93c35c4d155bcb8cb5d
victornovik <victor.novik@mail.ru>
2026-04-19 18:11:41 +0300

    Apply <NoWarn>CA1515</NoWarn> to Directory.Build.props to get rid of warnings

--- 4-cqrs/tests/ArchitectureTests/BaseTest.cs
+++ 4-cqrs/tests/ArchitectureTests/BaseTest.cs
@@ -3,11 +3,7 @@ using SharedKernel;
 
 namespace ArchitectureTests;
 
-#pragma warning disable CA1515
-
 public abstract class BaseTest
 {
     protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
 }
-
-#pragma warning restore CA1515
